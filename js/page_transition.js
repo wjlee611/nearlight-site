360505f2107763080cd3b2b43273e6dc242f0eac
wjlee611 <wjlee611@gmail.com>
2022-05-14 21:06:22 +0900

    improve loading speed

--- js/page_transition.js
+++ js/page_transition.js
@@ -30,19 +30,22 @@ function pageTransition(nodeList) {
   });
 }
 
-$(document).ready(function () {
+//loading
+$(function () {
   document.body.classList.add("reveal");
+});
 
+//DOM loaded
+$(document).ready(function () {
   document.getElementById("aniOnLoad").href = "";
   document.getElementById("top-line1").classList.add("index-loading__top-line");
   document.getElementById("top-line2").classList.add("index-loading__top-line");
+
+  pageTransition(document.querySelectorAll("a"));
 });
 
-window.onload = function () {
-  setTimeout(() => {
-    pageTransition(document.querySelectorAll("a"));
-
-    document.getElementById("aniOnLoad").href =
-      "css/components/index__bar--on-load.css";
-  }, 1);
-};
+//All resources loaded
+$(window).on("load", function () {
+  document.getElementById("aniOnLoad").href =
+    "css/components/index__bar--on-load.css";
+});
