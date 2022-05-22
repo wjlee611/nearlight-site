a1f4403c1be000245abd046de345f0a1882f92b1
wjlee611 <wjlee611@gmail.com>
2022-05-22 17:13:12 +0900

    improving loading ani and resource management

---
+++ js/page_transition_before_load.js
@@ -0,0 +1,40 @@
+function isSameAsLocation(uriString) {
+  const uri = new URL(uriString);
+
+  return (
+    uri.origin === window.location.origin &&
+    uri.pathname === window.location.pathname
+  );
+}
+
+function pageTransition(nodeList) {
+  nodeList.forEach((node) => {
+    if (!(node instanceof HTMLAnchorElement)) return;
+
+    const { href } = node;
+
+    if (!href || node.target === "_blank" || isSameAsLocation(href)) return;
+
+    node.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      document.body.addEventListener(
+        "transitionend",
+        () => {
+          window.location.href = href;
+        },
+        { passive: true, once: true }
+      );
+      document.body.classList.add("hidden");
+    });
+  });
+}
+
+setTimeout(() => {
+  document.body.classList.add("reveal");
+
+  pageTransition(document.querySelectorAll("a"));
+
+  document.getElementById("aniOnLoad").href =
+    "css/components/index__bar--on-load.css";
+}, 1);
